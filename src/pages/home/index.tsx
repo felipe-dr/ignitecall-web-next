73995687000d749139c7dc5a3159248a4fb5f833
Felipe DR <felipe.corp7@gmail.com>
2026-03-07 13:43:06 -0300

    feat: class-02 ClaimUsernameForm component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,10 @@
 import { Heading, Text } from '@ignite-ui/react'
 import Image from 'next/image'
 
+import previewImage from '../../assets/app-preview.png'
 import { Container, Hero, Preview } from './styles'
 
-import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
 export default function Home() {
   return (
@@ -16,6 +17,8 @@ export default function Home() {
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </Text>
+
+        <ClaimUsernameForm />
       </Hero>
 
       <Preview>
